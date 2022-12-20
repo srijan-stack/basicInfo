d755acedf3e338fcb194021bad8b4c59d48819d4
srijan-stack <52280610+srijan-stack@users.noreply.github.com>
2022-12-20 16:22:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import setup
 setup(name="basicinfo",
-version="0.1",
+version="0.2",
 url='https://github.com/srijan-stack/basicinfo',
 description="this is a info getter",
 long_description="use this to get info",
